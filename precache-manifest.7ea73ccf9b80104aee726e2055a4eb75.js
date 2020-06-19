e398d17c246262b61b12a5cb504efbd3de2e5470
rumothy <rumothy@yahoo.com>
2020-06-19 01:01:34 -0400

    Updates

---
+++ precache-manifest.7ea73ccf9b80104aee726e2055a4eb75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71f863c1961e0de61c18a546c7902af7",
+    "url": "/gitsearch/index.html"
+  },
+  {
+    "revision": "2314fe1e3423b8b54e25",
+    "url": "/gitsearch/static/css/main.48fd38af.chunk.css"
+  },
+  {
+    "revision": "2a95699fd965adc7f32c",
+    "url": "/gitsearch/static/js/2.3a2d5c26.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/gitsearch/static/js/2.3a2d5c26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2314fe1e3423b8b54e25",
+    "url": "/gitsearch/static/js/main.4d5b5224.chunk.js"
+  },
+  {
+    "revision": "39739aad09bc3e58cc23",
+    "url": "/gitsearch/static/js/runtime-main.dac7df62.js"
+  }
+]);
